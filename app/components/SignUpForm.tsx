7d1fb3397d6404af35819ee42dd75099039aa7f6
Anton Karpov <anton.karpov@rossvideo.com>
2026-06-12 17:17:54 -0400

    SignUpForm draft ready

--- app/components/SignUpForm.tsx
+++ app/components/SignUpForm.tsx
@@ -5,32 +5,60 @@ import { FormEvent, useState } from "react";
 import { FieldError, Form, Input, Label, TextField } from "@heroui/react";
 import { EMAIL_PLACEHOLDER } from "../constants";
 import { useForm } from "@tanstack/react-form";
+import { useAppForm } from "../forms";
+import z from "zod";
+
+type SignUpFormValues = z.infer<typeof signUpSchema>;
 
 export function SignUpForm() {
   const form = useForm({
     validators: {
       onChange: signUpSchema,
     },
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: ({ value }) => {
+      console.log(value);
+    },
   });
+  const { Field } = form;
 
-  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    console.log(event.currentTarget);
-  };
-
-  const [verifying, setVerifying] = useState(false);
+  // const [verifying, setVerifying] = useState(false);
 
   return (
-    <Form onSubmit={onSubmit}>
-      <TextField name="email" type="email">
-        <Label>Email</Label>
-        <Input placeholder={EMAIL_PLACEHOLDER} />
-        <FieldError />
-      </TextField>
-      <TextField name="password" type="password">
-        <Label>Password</Label>
-        <Input />
-        <FieldError />
-      </TextField>
+    <Form onSubmit={form.handleSubmit}>
+      <Field
+        name="email"
+        children={({ state, handleChange, handleBlur }) => (
+          <TextField
+            type="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={state.value}
+          >
+            <Label>Email</Label>
+            <Input placeholder={EMAIL_PLACEHOLDER} />
+            <FieldError />
+          </TextField>
+        )}
+      />
+      <Field
+        name="password"
+        children={({ state, handleChange, handleBlur }) => (
+          <TextField
+            type="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={state.value}
+          >
+            <Label>Password</Label>
+            <Input placeholder={EMAIL_PLACEHOLDER} />
+            <FieldError />
+          </TextField>
+        )}
+      />
     </Form>
   );
 }
